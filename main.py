205c7be04d7290c3fe82663649661818f3a6e58c
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2020-02-21 15:20:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -42,6 +42,21 @@ def on_release(key):
 #    on_release=on_release)
 #listenerk.start()
 
+def first_act():
+    sl(0.2)
+    movc("main_fig")
+    sl(1)
+    movc(figs[1])
+    sl(1)
+    movc(divs[3])
+    find_place(8)
+    sl(0.4)
+    movc("enter")
+    sl(1)
+    movc(lvls[8])
+    sl(3)
+    movc("close")
+
 def mp():
     global mouse
     return mouse.position
@@ -64,7 +79,6 @@ def find_place(num):
         movc("left")
 
 def act():
-    movc("intr")
     sl(0.1)
     #movc("close")
     #sl(0.1)
@@ -72,35 +86,46 @@ def act():
     sl(0.3)
     movc(figs[1])
     movc(divs[3])
-    find_place(3)
-    sl(0.4)
-    movc("enter")
-    sl(0.2)
-    movc(lvls[8])
-    sl(3)
-    for i in []:#lvls:
-        movc(i, 0)
-        sl(1)
-
-    movc("close")
-
-def first_act():
-    movc("intr")
-    sl(0.2)
-    movc("main_fig")
-    sl(1)
-    movc(figs[1])
-    sl(1)
-    movc(divs[3])
     find_place(8)
     sl(0.4)
     movc("enter")
-    sl(1)
+    sl(0.3)
     movc(lvls[8])
     sl(3)
-    movc("close")
+    find_need()
+    movc("down")
+    movc(rows[3])
+    movc(rows[4])
+    movc("validate")
+    sl(2)
+    movc("notag")
+    movc("validate")
+    sl(10)
+    for i in []:#lvls:
+        movc(i, 0)
+        sl(1)
+    #movc("close")
+    pass
 
-m_lis = 1
+def find_need():
+    movc(rows[4])
+    pass
+
+def atac():
+    #sl(3)
+    movc("0attack")
+    sl(1)
+    movc("1attack")
+    sl(1)
+    movc(enms[0], 0)
+    sl(1)
+    movc(enms[1], 0)
+    sl(1)
+    movc(enms[2], 0)
+
+m_lis = 0
+fir = 0
+
 mouse = Controller()
 if bool(m_lis) is True:
     print("pos mode")
@@ -128,6 +153,10 @@ pos_s = {
     "back":(340, 760),
     "left":(350, 470),
     "righ":(850, 470),
+    "0attack":(570, 780),
+    "1attack":(730, 780),
+    "validate":(630, 750),
+    "notag":(800, 700),
     }
 for i in range(len(figs)): pos_s[figs[i]] = (360+255*i, 725)
 for i in range(len(divs)): pos_s[divs[i]] = (265+110.5*i, 730)
@@ -135,9 +164,18 @@ for i in range(len(rows)): pos_s[rows[i]] = (280, 330+83*i)
 for i in range(3):
     for j in range(3):
         pos_s[lvls[i+j*3]] = (400+200*i, 355+100*j)
-
+pos_s[enms[0]] = (850, 320)
+pos_s[enms[1]] = (930, 410)
+pos_s[enms[2]] = (1020, 620)
 print(pos_s)
 
 sl(1)
-#first_act()
-act()
+if bool(fir) is True:
+    movc("intr")
+    first_act()
+    sl(0.5)
+    first_act()
+else:
+    movc("intr")
+    #act()
+    atac()
