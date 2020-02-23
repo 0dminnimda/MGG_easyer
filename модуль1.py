281f345aeca00d822d3748b0e712550139a31118
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2020-02-24 01:53:29 +0300

    Update модуль1.py

--- модуль1.py
+++ модуль1.py
@@ -2,13 +2,21 @@ import cv2 as cv
 from mss import mss
 import numpy as np
 
+def capture(size):
+    return np.array(sct.grab({'top': size[1],
+                       'left': size[0],
+                       'width': size[2],
+                       'height': size[3]}))
+
 sct = mss()
 
 whole_siz = [0, 225, 1500, 775]
 siz = [350, 865, 275, 132]
+ico_siz = [350, 865, 275, 132]
+link_siz = ico_siz
 
 while 1:
-    img = np.array(sct.grab({'top': siz[1], 'left': siz[0], 'width': siz[2], 'height': siz[3]}))
+    img = capture(link_siz)
 
     #cv.namedWindow("0", cv.WINDOW_NORMAL)
     cv.imshow("0", img)
@@ -16,3 +24,4 @@ while 1:
     if cv.waitKey(1) & 0xFF == ord('2'):
         cv.destroyAllWindows()
         break
+cv.imwrite("img.png", img)
