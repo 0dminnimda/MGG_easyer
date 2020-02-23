e88c1cf15a5a07bd5efd2b0dd94e77ab4507ab42
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2020-02-23 19:33:23 +0300

    Update speed_alg.py

--- speed_alg.py
+++ speed_alg.py
@@ -11,36 +11,20 @@ ratio = np.round_(speed/speed.min(), 2)
 num = 5
 
 print(speed)
-print(ratio)
-print(speed*ratio)
-print(speed/ratio)
-print(ratio/speed)
 for i in range(num):
     st_num += steps
     steps = np.trunc(ratio + rem)
     rem = ratio - steps
-    print(f"\n\n{i} step")
+    print(f"\n{i} step")
     arr = {}
     for m in range(speed.shape[0]):
-        print("[", end="")
-
         for n in range(int(steps[m])):
-            #print(n, end=" ")  # st_num[m]+
             a = n*speed[m]/(ratio[m])
             if a == 0:
                 a = speed[m]
-            print(a, end=" ")
             arr[str(m)+"_"+str(n)] = a
-
-        print("] ", end="")
     vals = list(arr.values())
     keys = list(arr.keys())
     sor = sorted(vals)[::-1]
-    #print("\n", arr, arr2)
     for i in sor:
-        print(keys[vals.index(i)], end=" ")
-    #print()
-    #print(steps)
-    #print(st_num)
-    #print(rem)
-    #print(sp)
+        print(list(keys[vals.index(i)])[0], end=" ")
